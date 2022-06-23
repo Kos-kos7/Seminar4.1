da1414ab07418b30fb7f7819242a6dac0a88f3e7
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-23 16:00:52 +0500

    сумма числе от 1 до Н через метод

---
+++ Task_24/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите целое число N => ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = Sum(number);
+Console.WriteLine($"Сумма чисел от 1 до N: {sum}");
+
+int Sum(int number)
+{
+int result = 0;
+for (int i = 1; i <= number; i++)
+{
+   result = result + i;
+}
+return result;
+}
