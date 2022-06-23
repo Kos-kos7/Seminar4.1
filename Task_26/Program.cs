336837737f485d8cbcae5186b994f98eebc74e93
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-23 16:27:52 +0500

    число цифр в числе через метод

---
+++ Task_26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите целое число => ");
+int number = int.Parse(Console.ReadLine());
+
+int quantityOfDigits = Count(number);
+
+Console.WriteLine($"Количество цифр в числе {number} => {quantityOfDigits}");
+
+int Count(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
